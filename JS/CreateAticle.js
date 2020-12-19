7bf2e721353affd758a74c2bbcd9871b2305cdad
TI2042 <tis3.1415@gmail.com>
2020-12-19 12:03:53 +0400

    Fix

--- JS/CreateAticle.js
+++ JS/CreateAticle.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let form = document.forms[0];
     let button = form["submit"];
 
-    button.addEventListener("click", (event) => {
+    button.addEventListener('click', (event) => {
         
 
         let formData = new FormData(form);
@@ -24,7 +24,6 @@ document.addEventListener("DOMContentLoaded", () => {
         if (!checkIfNotEmpty(formData.get('textArticle'))) return showMessage('error', 'Write what you want to share in the "Article" field.');
         if (!checkIfNotEmpty(formData.get('tags'))) return showMessage('error', 'Fill in the "Tags" field so others can easily find your article.');
 
-        let timerInterval
         Swal.fire({
             title: 'Loading',
             timer: 1000,
@@ -34,5 +33,5 @@ document.addEventListener("DOMContentLoaded", () => {
         showMessage('success', 'The article was successfully created! Congratulations.');
         
         event.preventDefault()
-    });
+    })
 })
