1ecde9c4f86f64da2695f49717d962065d8f47f8
TI2042 <tis3.1415@gmail.com>
2020-12-19 13:05:46 +0400

    test

--- JS/CreateAticle.js
+++ JS/CreateAticle.js
@@ -14,24 +14,23 @@ document.addEventListener("DOMContentLoaded", () => {
     let button = form["submit"];
     let formData = new FormData(form)
 
-    button.addEventListener("click", () => {
+    button.addEventListener("click", (event) => {
         event.preventDefault()
 
-    let formData = new FormData(form);
-    for (var value of formData.values()) {
-        console.log(value);
-    }
+        for (var value of formData.values()) {
+            console.log(value);
+        }
 
-    if (!checkIfNotEmpty(formData.get('articleTitle'))) return showMessage('error', 'Fill in the "Title" field.');
-    if (!checkIfNotEmpty(formData.get('textArticle'))) return showMessage('error', 'Write what you want to share in the "Article" field.');
-    if (!checkIfNotEmpty(formData.get('tags'))) return showMessage('error', 'Fill in the "Tags" field so others can easily find your article.');
+        if (!checkIfNotEmpty(formData.get('articleTitle'))) return showMessage('error', 'Fill in the "Title" field.');
+        if (!checkIfNotEmpty(formData.get('textArticle'))) return showMessage('error', 'Write what you want to share in the "Article" field.');
+        if (!checkIfNotEmpty(formData.get('tags'))) return showMessage('error', 'Fill in the "Tags" field so others can easily find your article.');
 
-    Swal.fire({
-        title: 'Loading',
-        timer: 1000,
-        timerProgressBar: true,
-        showConfirmButton: false,
-    })
-    showMessage('success', 'The article was successfully created! Congratulations.');
+        Swal.fire({
+            title: 'Loading',
+            timer: 1000,
+            timerProgressBar: true,
+            showConfirmButton: false,
+        })
+        showMessage('success', 'The article was successfully created! Congratulations.');
     })
 })
