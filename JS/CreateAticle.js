8ec2343daae6227ada60bca04def9462affb3584
TI2042 <tis3.1415@gmail.com>
2020-12-19 11:08:46 +0400

    add script

---
+++ JS/CreateAticle.js
@@ -0,0 +1,26 @@
+const form = document.forms[0];
+
+function checkIfNotEmpty(str) {
+    return str !== '';
+}
+function showMessage(type, text) {
+    Swal.fire({
+        text: text,
+        icon: type,
+        showConfirmButton: false
+    });
+}
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+	let formData = new FormData(form);
+	for (var value of formData.values()) {
+		console.log(value); 
+	}
+
+    if (!checkIfNotEmpty(formData.get('articleTitle'))) return showMessage('error', 'Fill in the "Title" field.');
+    if (!checkIfNotEmpty(formData.get('textArticle'))) return showMessage('error', 'Write what you want to share in the "Article" field.');
+    if (!checkIfNotEmpty(formData.get('tags'))) return showMessage('error', 'Fill in the "Tags" field so others can easily find your article.');
+
+    showMessage('success', 'The article was successfully created! Congratulations.');
+});
